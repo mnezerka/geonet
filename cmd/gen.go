dcb5bc5917384f7ff9035b7cf7558d3e3c903ad2
Michal Nezerka <michal.nezerka@gmail.com>
2025-01-30 21:58:49 +0100

    cmd: flag to limit number of tracks for gen command

--- cmd/gen.go
+++ cmd/gen.go
@@ -10,6 +10,7 @@ import (
 )
 
 var genCmdInterpolate bool
+var genCmdLimit int
 
 var genCmd = &cobra.Command{
 	Use:   "gen",
@@ -26,6 +27,13 @@ var genCmd = &cobra.Command{
 		log.Infof("generating geonet from %v", args)
 
 		for file_ix := 0; file_ix < len(args); file_ix++ {
+
+			// interrupt loop if number of tracks is limited
+			if genCmdLimit >= 0 {
+				if file_ix >= genCmdLimit {
+					break
+				}
+			}
 
 			log.Infof("processing %s", args[file_ix])
 
@@ -49,6 +57,7 @@ func init() {
 	genCmd.PersistentFlags().BoolVarP(&genCmdInterpolate, "interpolate", "i", false, "interpolate tracks before adding to geonet")
 	genCmd.PersistentFlags().Int64Var(&config.Cfg.InterpolationDistance, "int-dist", config.Cfg.InterpolationDistance, "distance for interpolation")
 	genCmd.PersistentFlags().Int64Var(&config.Cfg.MatchMaxDistance, "match-max-dist", config.Cfg.MatchMaxDistance, "maximal distance for matching new points against points in geonet")
+	genCmd.PersistentFlags().IntVar(&genCmdLimit, "limit", -1, "max number of tracks to be processed")
 
 	rootCmd.AddCommand(genCmd)
 }
